f9d4b0f30f90a286cd312d4b9d6cacb46265ca66
Dev-GiaPhu <jemnguyen114@gmail.com>
2026-09-09 17:37:16 +0700

    Set Donate message

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,3 @@
 export const FIREBASE={apiKey:'AIzaSyB7y9r8iAugTHBUQwGbyiE4XHrQ0lZHyuY',authDomain:'gp-statistical-public.firebaseapp.com',projectId:'gp-statistical-public',storageBucket:'gp-statistical-public.firebasestorage.app',messagingSenderId:'38954339425',appId:'1:38954339425:web:5638ef4f4e0a038b47ee20',measurementId:'G-GZXE826P6X'};
 export const SERVICES={sendVerification:'REPLACE_SEND_VERIFICATION_URL',register:'REPLACE_REGISTER_URL',scanDrive:'REPLACE_SCAN_DRIVE_URL'};
-export const DONATE={qr:'',text:'Cảm ơn bạn đã ủng hộ và giúp dự án tiếp tục được duy trì.'};
+export const DONATE={qr:'',text:'Nếu bạn thấy công cụ này hữu ích, bạn có thể ủng hộ tôi để tiếp tục duy trì và phát triển nó.'};
