34cc73a58305efbc7c7f8b5305f0b0c68a4b451e
Dev-GiaPhu <jemnguyen114@gmail.com>
2026-09-09 22:24:51 +0700

    Use original donation QR image

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 export const FIREBASE={apiKey:'AIzaSyB7y9r8iAugTHBUQwGbyiE4XHrQ0lZHyuY',authDomain:'gp-statistical-public.firebaseapp.com',projectId:'gp-statistical-public',storageBucket:'gp-statistical-public.firebasestorage.app',messagingSenderId:'38954339425',appId:'1:38954339425:web:5638ef4f4e0a038b47ee20',measurementId:'G-GZXE826P6X'};
 export const EMAILJS={publicKey:'OoSqsm62BM1dhdS5W',serviceId:'service_83ktn7r',templateId:'template_d9ln28q'};
 export const DRIVE={apiKey:'',maxDepth:8,maxFiles:500};
-export const DONATE={qr:'assets/donate-qr.svg',text:'Nếu bạn thấy công cụ này hữu ích, bạn có thể ủng hộ tôi để tiếp tục duy trì và phát triển nó.'};
+export const DONATE={qr:'assets/donate-qr.jpg',text:'Nếu bạn thấy công cụ này hữu ích, bạn có thể ủng hộ tôi để tiếp tục duy trì và phát triển nó.'};
 export const APP={name:'GP Statistical',version:'2.0.0'};
